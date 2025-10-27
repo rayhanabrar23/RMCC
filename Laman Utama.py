d78d441bf3f310362afb7b6fdc1d0a46811a4230
rayhanabrar23 <rayhanabrar023@gmail.com>
2025-10-27 13:49:33 +0700

    Update Laman Utama.py

--- Laman Utama.py
+++ Laman Utama.py
@@ -1,4 +1,4 @@
-# Laman Utama.py (File Utama/Index - VERSI AKHIR DAN BERSIH)
+# Laman Utama.py (File Utama/Index - FINAL)
 
 import streamlit as st
 
@@ -8,27 +8,23 @@ def main():
     
     logo_url = "https://www.pei.co.id/images/logo-grey-3x.png" 
     
-    # -----------------------------------------------------------
     # KODE STABIL UNTUK MENEMPATKAN LOGO DI PALING ATAS
-    # -----------------------------------------------------------
     # Menggunakan fitur bawaan st.logo()
     st.logo(logo_url, icon_image=None) 
-    
-    # -----------------------------------------------------------
     
     # Konten halaman utama Anda
     st.title("📊 LL & CL Report Generator Dashboard")
     st.markdown("""
-    Selamat datang! Aplikasi ini telah dibagi menjadi dua halaman (aplikasi) terpisah yang dapat Anda akses melalui **menu navigasi di sebelah kiri** (ikon **>**).
+    Selamat datang! Aplikasi ini telah dibagi menjadi tiga halaman (aplikasi) terpisah yang dapat Anda akses melalui **menu navigasi di sebelah kiri** (ikon **>**).
 
     Pilih halaman:
     
     - **Lendable Limit (LL):** Untuk menghitung posisi Lendable Limit.
     - **Concentration Limit (CL):** Untuk menghitung Batas Konsentrasi dan Haircut.
+    - **UMA Scraper:** Untuk mengambil data Unusual Market Activity dari IDX.
 
-    _Setiap halaman memerlukan set file input yang berbeda._
+    _Setiap halaman LL & CL memerlukan set file input yang berbeda._
     """)
 
 if __name__ == '__main__':
     main()
-

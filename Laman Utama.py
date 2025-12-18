4aa178e44b3f34b580f3f45e94f3b4aa7883466a
rayhanabrar23 <rayhanabrar023@gmail.com>
2025-12-18 10:13:07 +0700

    Update Laman Utama.py

--- Laman Utama.py
+++ Laman Utama.py
@@ -15,7 +15,7 @@ def get_base64(bin_file):
 
 # Ganti 'background.jpg' dengan nama file gambar aslimu
 try:
-    bin_str = get_base64('backgorund.jpg') 
+    bin_str = get_base64('background.jpg') 
     bg_img_style = f"background-image: url('data:image/png;base64,{bin_str}');"
 except FileNotFoundError:
     # Jika gambar tidak ketemu, fallback ke warna hitam seperti awal
@@ -104,5 +104,6 @@ elif st.session_state.get("authentication_status") is False:
 
 elif st.session_state.get("authentication_status") is None:
     st.warning('Silakan masukkan kredensial untuk mengakses dashboard.')
+
 
 
